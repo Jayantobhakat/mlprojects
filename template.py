9084930483226372cf16f173094e22e83e1b16c1
Jayanto Bhakat <er.jayanto.mec@gmail.com>
2024-01-19 02:09:59 +0530

    dvc init

--- template.py
+++ template.py
@@ -4,21 +4,21 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-project_name="mlproject"
+project_name="mlproject1"
 
 list_of_files=[
-    f"src/(project_name)/__init__.py",
-    f"src/(project_name)/components/__init__.py",
-    f"src/(project_name)/components/data_ingestion.py",
-    f"src/(project_name)/components/data_transformation.py",
-    f"src/(project_name)/components/model_transfer.py",
-    f"src/(project_name)/components/model_monitering.py",
-    f"src/(project_name)/pipelines/__init__.py",
-    f"src/(project_name)/pipelines/training_pipeline.py",
-    f"src/(project_name)/pipelines/prediction_pipeline.py",
-    f"src/(project_name)/exception.py",
-    f"src/(project_name)/logger.py",
-    f"src/(project_name)/utils.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/model_transfer.py",
+    f"src/{project_name}/components/model_monitering.py",
+    f"src/{project_name}/pipelines/__init__.py",
+    f"src/{project_name}/pipelines/training_pipeline.py",
+    f"src/{project_name}/pipelines/prediction_pipeline.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/utils.py",
     "main.py",
     "app.py",
     "Dockerfile",
